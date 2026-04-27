1922baa36bb4f7597464094c36268bc6474e90df
Srijan-61 <bhandarisrijan321@gmail.com>
2026-04-27 18:29:25 +0545

    implement JWT authentication and login service

--- VehicleInventoryManagementSystem.Application/DTOs/DTO.cs
+++ VehicleInventoryManagementSystem.Application/DTOs/DTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace VehicleInventoryManagementSystem.Application.DTOs
@@ -12,4 +13,22 @@ namespace VehicleInventoryManagementSystem.Application.DTOs
         public string Password { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
     }
+
+    public class LoginDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public int AccessTokenExpirationMinutes { get; set; }
+    }
+
 }
---
+++ VehicleInventoryManagementSystem.Infrastructure/Repositories/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleInventoryManagementSystem.Infrastructure.Repositories
+{
+    internal class Class1
+    {
+    }
+}
